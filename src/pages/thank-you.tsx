aacdaed88b54d389d830783050ed84cb96956711
Heidi Williams-Foy <foywilliams@gmail.com>
2023-05-11 22:00:03 -0400

    fixed Navbar issue

--- src/pages/thank-you.tsx
+++ src/pages/thank-you.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { HiCheckCircle } from "react-icons/hi";
-import Navbar from "../components/Navbar";
+import Navbar from "../app/components/Navbar";
 
 const ThankYou = () => {
     const router = useRouter();
